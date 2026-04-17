0c9f4d1f6306b1f99a55f87b9e05000d9ae7431b
Diana <164666358+DianaxSan@users.noreply.github.com>
2026-04-17 18:29:34 -0500

    fix orden app express

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const app = express();
+
 const db = require('./db');
 const PORT = process.env.PORT || 3000;
 
